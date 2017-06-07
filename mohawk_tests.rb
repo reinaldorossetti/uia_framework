cdebd248a154054764df8feb88796b3069ab2e5c
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2017-06-07 18:22:12 -0300

    Add files via upload

---
+++ mohawk_tests.rb
@@ -0,0 +1,21 @@
+require 'mohawk'
+
+class LoginScreen
+  include Mohawk
+  window(:title => /HP/)
+  #text(:username, :id => "agentName")
+  text(:username, :id => "agentName")
+  text(:password, :id => "password")
+  button(:login, :name => "OK")
+end
+
+
+def login
+  test = LoginScreen.new
+  test.username.clear
+  test.username = "john"
+  test.password = "HP"
+  test.login
+end
+
+login
